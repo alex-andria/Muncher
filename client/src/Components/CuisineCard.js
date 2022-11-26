53fc4488666c775f7175faf75fd259f6fe39cbb3
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-11-25 23:55:00 -0500

    Cuisine Card created

---
+++ client/src/Components/CuisineCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CuisineCard(){
+
+    return(
+        <>
+            <h1>Cuisine Name Here</h1>
+            <p>Image Here</p>
+        </>
+    );
+}
+
+export default CuisineCard;
