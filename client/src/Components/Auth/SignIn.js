5a01c7564481911c968d4d226792f3b9fce6fed5
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-11-27 19:35:35 -0500

    sign-in and sign-up form handling complete except for fetch request

--- client/src/Components/Auth/SignIn.js
+++ client/src/Components/Auth/SignIn.js
@@ -1,81 +1,176 @@
-import React from "react";
-import style from'./Auth.css';
+import React, { useState, useEffect } from "react";
+import style from "./Auth.css";
 
-function SignIn(){
-    // const [username, setUsername] = useState("");
-    // const [paassword, setPassword] = useState("");
-    // const [errors, setErrors] = useState([]); 
+function SignIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [newUserUsername, setNewUserUsername] = useState("");
+  const [newUserPassword, setNewUserPassword] = useState("");
+  const [newUserPasswordConfirmation, setNewUserPasswordConfirmation] =
+    useState("");
+  const [errors, setErrors] = useState([]);
 
-    // const [showSignUp, setShowSignup] = useState(false);
-    
-    //log-in submit
-    function handleSignInSubmit(e){
+  //log-in submit
+  function handleSignInSubmit(e) {
+    e.preventDefault();
 
-    }
+    const login = {
+      username: username,
+      password: password,
+    };
+    console.log(login);
 
-    //sign-up submit
-    function handleSignUpSubmit(e){
+    // ***** POST request for user log-in *****
+    // fetch("http://localhost:3000/login", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(login),
+    //   }).then((data) => {
+    //     if (data.ok) {
+    //       data.json().then((user) => onLogin(user));
+    //     } else {
+    //       data.json().then((err) => setErrors(err.errors));
+    //     }
+    //   });
+  }
 
-    }
+  //sign-up submit
+  function handleSignUpSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      username: newUserUsername,
+      password: newUserPassword,
+      password_confirmation: newUserPasswordConfirmation,
+    };
+    console.log(newUser);
+  }
 
-    return(
-        <>
-            <div className="login-wrap">
-                <div className="login-html">
-                    <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked/><label htmlFor="tab-1" className="tab">Sign In</label>
-                    <input id="tab-2" type="radio" name="tab" className="sign-up"/><label htmlFor="tab-2" className="tab">Sign Up</label>
-                    <div className="login-form">
-                        <div className="sign-in-htm">
-                            <div className="group">
-                                <label htmlFor="user" className="label">Username</label>
-                                <input id="user" type="text" className="input"/>
-                            </div>
-                            <div className="group">
-                                <label htmlFor="pass" className="label">Password</label>
-                                <input id="pass" type="password" className="input" data-type="password"/>
-                            </div>
-                            {/* <div className="group">
-                                <input id="check" type="checkbox" className="check" checked/>
-                                <label htmlFor="check"><span className="icon"></span> Keep me Signed in</label>
-                            </div> */}
-                            <div className="group">
-                                <input type="submit" className="button" value="Sign In"/>
-                            </div>
-                            <div className="hr"></div>
-                            {/* <div className="foot-lnk">
-                                <a href="#forgot">Forgot Password?</a>
-                            </div> */}
-                        </div>
-                        <div className="sign-up-htm">
-                            <div className="group">
-                                <label htmlFor="user" className="label">Username</label>
-                                <input id="user" type="text" className="input"/>
-                            </div>
-                            <div className="group">
-                                <label htmlFor="pass" className="label">Password</label>
-                                <input id="pass" type="password" className="input" data-type="password"/>
-                            </div>
-                            <div className="group">
-                                <label htmlFor="pass" className="label">Repeat Password</label>
-                                <input id="pass" type="password" className="input" data-type="password"/>
-                            </div>
-                            <div className="group">
-                                <label htmlFor="pass" className="label">Email Address</label>
-                                <input id="pass" type="text" className="input"/>
-                            </div>
-                            <div className="group">
-                                <input type="submit" className="button" value="Sign Up"/>
-                            </div>
-                            <div className="hr"></div>
-                            <div className="foot-lnk">
-                                <label htmlFor="tab-1">Already Member?</label>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <div className="login-wrap">
+        <div className="login-html">
+          <input
+            id="tab-1"
+            type="radio"
+            name="tab"
+            className="sign-in"
+            defaultChecked
+          />
+          <label htmlFor="tab-1" className="tab">
+            Sign In
+          </label>
+          <input id="tab-2" type="radio" name="tab" className="sign-up" />
+          <label htmlFor="tab-2" className="tab">
+            Sign Up
+          </label>
+          <div className="login-form">
+
+            {/* log-in form */}
+            <form onSubmit={handleSignInSubmit}>
+              <div className="sign-in-htm">
+                <div className="group">
+                  <label htmlFor="user" className="label">
+                    Username
+                  </label>
+                  <input
+                    id="user"
+                    type="text"
+                    className="input"
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
                 </div>
-            </div>
-        </>
-    );
+                <div className="group">
+                  <label htmlFor="pass" className="label">
+                    Password
+                  </label>
+                  <input
+                    id="pass"
+                    type="password"
+                    className="input"
+                    data-type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                {/* <div className="group">
+                  <input id="check" type="checkbox" className="check" checked />
+                  <label htmlFor="check">
+                    <span className="icon"></span> Keep me Signed in
+                  </label>
+                </div> */}
+                <div className="group">
+                  <input type="submit" className="button" value="Sign In" />
+                </div>
+                <div className="hr"></div>
+                {/* <div className="foot-lnk">
+                  <a href="#forgot">Forgot Password?</a>
+                </div> */}
+              </div>
+            </form>
+
+            {/* sign-up form */}
+            <form onSubmit={handleSignUpSubmit}>
+              <div className="sign-up-htm">
+                <div className="group">
+                  <label htmlFor="user" className="label">
+                    Username
+                  </label>
+                  <input
+                    id="user"
+                    type="text"
+                    className="input"
+                    onChange={(e) => setNewUserUsername(e.target.value)}
+                  />
+                </div>
+                <div className="group">
+                  <label htmlFor="pass" className="label">
+                    Password
+                  </label>
+                  <input
+                    id="pass"
+                    type="password"
+                    className="input"
+                    data-type="password"
+                    onChange={(e) => setNewUserPassword(e.target.value)}
+                  />
+                </div>
+                <div className="group">
+                  <label htmlFor="pass" className="label">
+                    Repeat Password
+                  </label>
+                  <input
+                    id="pass"
+                    type="password"
+                    className="input"
+                    data-type="password"
+                    onChange={(e) =>
+                      setNewUserPasswordConfirmation(e.target.value)
+                    }
+                  />
+                </div>
+                {/* ***** Email Address ***** */}
+                {/* <div className="group">
+                  <label htmlFor="pass" className="label">
+                    Email Address
+                  </label>
+                  <input id="pass" type="text" className="input" />
+                </div> */}
+                <div className="group">
+                  <input type="submit" className="button" value="Sign Up" />
+                </div>
+                <div className="hr"></div>
+                <div className="foot-lnk">
+                  <label htmlFor="tab-1">Already Member?</label>
+                </div>
+              </div>
+            </form>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SignIn;
