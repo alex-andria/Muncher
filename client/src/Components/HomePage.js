423002ee08bac912236fa06bf81d04cc0c72a83b
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-12-15 23:57:03 -0500

    testing get code route

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -3,31 +3,37 @@ import { useState, useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const [roomCode, setRoomCode] = useState("")
   const navigate = useNavigate();
 
   const navigateCreateRoom = () => {
     // 👇️ navigate to /
-    navigate("/create-room");
+    // navigate("/create-room");
+    navigate("/create-room", { state: { roomCode: roomCode } });
   };
 
 
   // ** test code for simplifying create button **
-  // function handleCreateCodeButton() {
-  //   fetch("/api/room", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     // body: JSON.stringify(data),
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setRoomCode(data.code);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error:", error);
-  //     });
-  // }
+  function handleCreateCodeButton() {
+    fetch("/api/room", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setRoomCode(data.code);
+        console.log(roomCode);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    
+    navigate("/create-room", { state: { roomCode: roomCode } });
+    
+  }
 
   const navigateJoinRoom = () => {
     // 👇️ navigate to /
@@ -36,7 +42,7 @@ function HomePage() {
 
   return (
     <>
-      <button className="buttons-room" type="button" onClick={navigateCreateRoom}>
+      <button className="buttons-room" type="button" onClick={handleCreateCodeButton}>
         Create Room
       </button>
       <br /> <br /> <br />
