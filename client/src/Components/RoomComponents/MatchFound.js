31870f7396113c892563b18f477c4cd49d0f84ec
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-12-25 23:59:21 -0500

    confetti feature added on match

--- client/src/Components/RoomComponents/MatchFound.js
+++ client/src/Components/RoomComponents/MatchFound.js
@@ -1,17 +1,21 @@
 import React, {useState, useEffect} from "react";
+import ConfettiExplosion from 'react-confetti-explosion';
 import TinderCard from "react-tinder-card";
 
 function MatchFound({match}){
+    // const [isExploding, setIsExploding] = useState(true);
     console.log(match);
     let url = `url(./CuisineImages/${match}.jpg)`;
     console.log(url);
 
     //insert confetti here
-    
+
     return (
         <div>  
             <h1>You matched on {match} cuisine!</h1>
             <div  className="cardContainer">
+            {/* {isExploding && <ConfettiExplosion />} */}
+            <ConfettiExplosion/>
             <TinderCard
             //   ref={childRefs[index]}
               className="swipe"
